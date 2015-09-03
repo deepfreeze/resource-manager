8684d4780f7fc16f1119208847d4a16f5ff58804
Ice Wolf <github@deep-freeze.ca>
2015-09-03 13:49:24 -0700

    Add Exception Interface.

---
+++ src/Exception/ExceptionInterface.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace DeepFreeze\Intl\Resource;
+
+use DeepFreezeSpi\Intl\Resource\ExceptionInterface as SpiExceptionInterface;
+
+interface ExceptionInterface extends SpiExceptionInterface {
+
+}
