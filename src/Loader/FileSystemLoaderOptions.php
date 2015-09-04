bb9981b2c6c9d9ad845a44c4832d1b99661990b4
Ice Wolf <github@deep-freeze.ca>
2015-09-04 11:43:54 -0700

    Filesystem Loader Options.

--- src/Loader/FileSystemLoaderOptions.php
+++ src/Loader/FileSystemLoaderOptions.php
@@ -3,5 +3,23 @@
 namespace DeepFreeze\Intl\Resource\Loader;
 
 class FileSystemLoaderOptions {
+  /**
+   * @var string
+   */
+  private $filename;
+  /**
+   * @var string[]
+   */
+  private $templates;
+  /**
+   * @var string
+   */
+  private $basePath;
+
+  /**
+   * @var static
+   */
+  private $inheritedOptions;
+
 
 }
---
+++ test/src/Loader/FileSystemLoaderOptionsTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace DeepFreezeTest\Intl\Resource\Loader;
+
+use DeepFreeze\Intl\Resource\Loader\FileSystemLoader;
+use DeepFreeze\Intl\Resource\Loader\FileSystemLoaderOptions;
+use PHPUnit_Framework_TestCase as TestCase;
+
+class FileSystemLoaderOptionsTest extends TestCase {
+  public function testFilenameMutators() {
+    $options = new FileSystemLoaderOptions();
+    $this->markTestIncomplete();
+  }
+}
